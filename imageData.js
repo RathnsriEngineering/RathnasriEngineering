4aa9d9b8fd4ff7a9e7ce47989669af9f6b66efc7
RathnsriEngineering <148851660+RathnsriEngineering@users.noreply.github.com>
2024-09-14 16:29:56 +0530

    Update imageData.js

--- imageData.js
+++ imageData.js
@@ -1,3 +1,3 @@
 const imageData = [
-    { src: 'images/image1.jpeg', caption: 'Image 1 Caption', link: 'https://example.com/1' }
+    { src: 'images/image1.jpeg', caption: 'Helmholtz Free Energy - Equation, Significance, and Applications: testbook.com', link: 'https://testbook.com/physics/helmholtz-free-energy#:~:text=Helmholtz%20free%20energy%20is%20a%20thermodynamic%20that%20measures%20the%20work,the%20greatest%20amount%20of%20work.' }
 ];
