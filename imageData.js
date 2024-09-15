80395642aa5549474cf2bfd09b812df906e49e4e
RathnsriEngineering <148851660+RathnsriEngineering@users.noreply.github.com>
2024-09-15 12:26:29 +0530

    Update imageData.js

--- imageData.js
+++ imageData.js
@@ -1,3 +1,4 @@
 const imageData = [
+    { src: 'images/image2.jpeg', caption: 'Jet Fuel From Farm Waste- Byogy Renewables Inc.', link: 'https://optimation.us/projects/jet-fuel-from-farm-waste/' },
     { src: 'images/image1.jpeg', caption: 'Helmholtz Free Energy - Equation, Significance, and Applications: testbook.com', link: 'https://testbook.com/physics/helmholtz-free-energy#:~:text=Helmholtz%20free%20energy%20is%20a%20thermodynamic%20that%20measures%20the%20work,the%20greatest%20amount%20of%20work.' }
 ];
