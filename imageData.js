0687fbddf732ae3e1623fefab0850ee660e71e44
RathnsriEngineering <148851660+RathnsriEngineering@users.noreply.github.com>
2024-09-18 16:44:40 +0530

    Update imageData.js

--- imageData.js
+++ imageData.js
@@ -1,5 +1,6 @@
 const imageData = [
     { src: 'images/image5.jpeg', caption: 'What is Chemical and Process Engineering?- uom.lk', link: 'https://uom.lk/cpe/about-us/what-is-chemical-engineering' },
+    { src: 'images/image6.jpeg', caption: 'Basic steps of the winemaking process- finding.wine', link: 'https://finding.wine/blogs/blog-posts/basic-steps-of-the-winemaking-process?srsltid=AfmBOoqToQgq8pE0x0MY_4t8IzJdWQmc7ID37sh3lowS_z9pVJHYyJIn' },
     { src: 'images/image4.jpeg', caption: 'Why counter flow heat exchangers are more efficient?- thermex.co.uk', link: 'https://www.thermex.co.uk/news/blog/605-why-counter-flow-heat-exchangers-are-more-efficient' },
     { src: 'images/image3.jpeg', caption: 'What is a heat exchanger?- thermex.co.uk', link: 'https://www.thermex.co.uk/news/blog/160-what-is-a-heat-exchanger' },
     { src: 'images/image2.jpeg', caption: 'Jet Fuel From Farm Waste- Byogy Renewables Inc.', link: 'https://optimation.us/projects/jet-fuel-from-farm-waste/' },
