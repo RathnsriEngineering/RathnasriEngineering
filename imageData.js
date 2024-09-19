d42e2772d9bd05bac8d0014334a21bb7377ebab5
RathnsriEngineering <148851660+RathnsriEngineering@users.noreply.github.com>
2024-09-19 11:39:20 +0530

    Update imageData.js

--- imageData.js
+++ imageData.js
@@ -1,5 +1,8 @@
 const imageData = [
     { src: 'images/image5.jpeg', caption: 'What is Chemical and Process Engineering?- uom.lk', link: 'https://uom.lk/cpe/about-us/what-is-chemical-engineering' },
+    { src: 'images/image9.jpeg', caption: 'What is Agricultural Biotechnology?- usda.gov', link: 'https://www.usda.gov/topics/biotechnology/biotechnology-frequently-asked-questions-faqs' },
+    { src: 'images/image8.jpeg', caption: 'Medical Biotechnology: Advancement and Ethics- wgu.edu', link: 'https://www.wgu.edu/blog/medical-biotechnology-advancements-ethics1811.html' },
+    { src: 'images/image7.jpeg', caption: 'What is Industrial Biotechnology?- archive.bio.org', link: 'https://archive.bio.org/articles/what-industrial-biotechnology' },
     { src: 'images/image6.jpeg', caption: 'Basic steps of the winemaking process- finding.wine', link: 'https://finding.wine/blogs/blog-posts/basic-steps-of-the-winemaking-process?srsltid=AfmBOoqToQgq8pE0x0MY_4t8IzJdWQmc7ID37sh3lowS_z9pVJHYyJIn' },
     { src: 'images/image4.jpeg', caption: 'Why counter flow heat exchangers are more efficient?- thermex.co.uk', link: 'https://www.thermex.co.uk/news/blog/605-why-counter-flow-heat-exchangers-are-more-efficient' },
     { src: 'images/image3.jpeg', caption: 'What is a heat exchanger?- thermex.co.uk', link: 'https://www.thermex.co.uk/news/blog/160-what-is-a-heat-exchanger' },
